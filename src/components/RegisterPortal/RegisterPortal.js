1791e0d80f9c314ddb6b04a69ed0b353402e3683
vanshkapoor <vanshkapoorvk7@gmail.com>
2019-06-22 23:48:43 +0530

    base phase-1

---
+++ src/components/RegisterPortal/RegisterPortal.js
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react'
+
+// export default class RegisterPortal extends Component {
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
